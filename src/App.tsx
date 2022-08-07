f596540de6aa03134e4fa1d52411db07f35486f6
= <31937542+iAverages@users.noreply.github.com>
2022-08-07 02:34:29 +0100

    sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [bonks, setBonks] = useState(null);
+    useEffect(() => {
+        fetch("whatever ur shit is")
+            .then((res) => res.json())
+            .then((res) => setBonks(res.whatever));
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={"https://cdn.danielraybone.com/screenshots/pfp.png"} className="App-logo" alt="logo" />
+                <p>NotWellard's bonk counter is currently at...</p>
+                <p className="bonks">{bonks ? bonks : "Loading"}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
